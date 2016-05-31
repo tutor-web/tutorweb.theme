678ee2be33f23d21158611d88210e974e228e3d4
Jamie Lentin <jm@lentin.co.uk>
2016-05-31 08:20:33 +0100

    Turn register & login links in banner into pop-up links too

--- tutorweb/theme/www.tutor-web.net/js/custom.js
+++ tutorweb/theme/www.tutor-web.net/js/custom.js
@@ -62,3 +62,36 @@
             anchor.onclick = sneaky.sneak;
         });
     });
+
+jQuery(function($){
+    var common_content_filter = '#content>*:not(div.configlet),dl.portalMessage.error,dl.portalMessage.info';
+
+    $('#banner-text a[href$="/register"]').prepOverlay({
+        subtype: 'ajax',
+        filter: common_content_filter,
+        cssclass: 'overlay-register',
+        formselector: 'form.kssattr-formname-register'
+    });
+
+    $('#banner-text a[href$="/login"]').prepOverlay({
+        subtype: 'ajax',
+        filter: common_content_filter,
+        formselector: 'form#login_form',
+        cssclass: 'overlay-login',
+        noform: function() {
+            if (location.href.search(/pwreset_finish$/) >= 0) {
+                return 'redirect'
+            } else {
+                return 'reload'
+            }
+        },
+        redirect: function() {
+            var href = location.href;
+            if (href.search(/pwreset_finish$/) >= 0) {
+                return href.slice(0, href.length - 14) + 'logged_in'
+            } else {
+                return href
+            }
+        }
+    });
+});
